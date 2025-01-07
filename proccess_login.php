7f71bd6e550c324cbdf42137cdfa6855ba947304
aleefandrian <aleefandriansusanto06@gmail.com>
2025-01-07 10:23:16 +0700

    Add files via upload

---
+++ proccess_login.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "bengkel");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password_verify = $_POST['password'];
+
+    $sql = "SELECT * FROM akun WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    
+    $stmt->bind_param("s", $username); 
+    
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password_verify, $user['password'])) {
+            
+            $_SESSION['id_akun'] = $user['id_akun'];
+            $_SESSION['username'] = $user['username'];
+
+                   header("Location: index.php");
+            }
+            exit(); 
+        } else {
+            echo "<script>alert('Password salah!');</script>";
+            echo "<script>window.location.href = 'login.php';</script>";
+            exit();
+        }
+    } else {
+        echo "<script>alert('Email tidak ditemukan!');</script>";
+        echo "<script>window.location.href = 'login.php';</script>";
+        exit();
+    }
+
+?>
